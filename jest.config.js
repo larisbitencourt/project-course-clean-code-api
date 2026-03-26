d79e46a10f14e6aff015c350936b51e4829a4338
Larissa Bitencourt <larissabitencourt95@gmail.com>
2026-03-26 10:31:43 -0300

    feat: ensure requests parser body as json

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,12 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^node:(.*)$': '$1'
   }
 }
